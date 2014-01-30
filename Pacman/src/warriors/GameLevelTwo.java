875d09ab4e8878a4ec815e168767751935bf41de
Stonesong <chans.pierre@gmail.com>
2014-01-30 18:26:31 +0100

    lvl2

---
+++ Pacman/src/warriors/GameLevelTwo.java
@@ -0,0 +1,146 @@
+package warriors;
+
+import gameframework.game.CanvasDefaultImpl;
+import gameframework.game.Game;
+import gameframework.game.GameLevelDefaultImpl;
+import gameframework.game.GameMovableDriverDefaultImpl;
+import gameframework.game.GameUniverseDefaultImpl;
+import gameframework.game.GameUniverseViewPortDefaultImpl;
+import gameframework.game.MoveBlockerChecker;
+import gameframework.game.MoveBlockerCheckerDefaultImpl;
+import gameframework.game.MoveBlockerRulesApplierDefaultImpl;
+import gameframework.game.OverlapProcessor;
+import gameframework.game.OverlapProcessorDefaultImpl;
+
+import java.awt.Canvas;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+
+import warriors.entity.Floor;
+import warriors.entity.GameSoldier;
+import warriors.entity.HolyGrailBonus;
+import warriors.entity.ShieldBonus;
+import warriors.entity.SwordBonus;
+import warriors.entity.Wall;
+import warriors.observers.GameSoldierObserver;
+import warriors.rule.WarriorMoveStrategy;
+import warriors.rule.WarriorsOverlapRules;
+
+public class GameLevelTwo extends GameLevelDefaultImpl {
+	Canvas canvas;
+
+	// 0 : Pacgums; 1 : Walls; 2 : SuperPacgums; 3 : Doors; 4 : Jail; 5 : empty
+	// Note: teleportation points are not indicated since they are defined by
+	// directed pairs of positions.
+	static int[][] tab = { 
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
+
+	public static final int SPRITE_SIZE = 16;
+
+	@Override
+	protected void init() {
+		OverlapProcessor overlapProcessor = new OverlapProcessorDefaultImpl();
+
+		MoveBlockerChecker moveBlockerChecker = new MoveBlockerCheckerDefaultImpl();
+		moveBlockerChecker.setMoveBlockerRules(new MoveBlockerRulesApplierDefaultImpl());
+
+		WarriorsOverlapRules overlapRules = new WarriorsOverlapRules(new Point(14 * SPRITE_SIZE, 17 * SPRITE_SIZE),
+				new Point(14 * SPRITE_SIZE, 15 * SPRITE_SIZE), life[0], score[0], endOfGame);
+		overlapProcessor.setOverlapRules(overlapRules);
+
+		universe = new GameUniverseDefaultImpl(moveBlockerChecker, overlapProcessor);
+		overlapRules.setUniverse(universe);
+
+		gameBoard = new GameUniverseViewPortDefaultImpl(canvas, universe);
+		((CanvasDefaultImpl) canvas).setDrawingGameBoard(gameBoard);
+
+		// Filling up the universe with basic non movable entities and inclusion in the universe
+		for (int i = 0; i < 31; ++i) {
+			for (int j = 0; j < 28; ++j) {
+				if (tab[i][j] == 0) {
+					universe.addGameEntity(new Floor(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+				}
+				if (tab[i][j] == 1) {
+					universe.addGameEntity(new Wall(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+				}
+				if (tab[i][j] == 2) {
+					universe.addGameEntity(new Floor(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+					universe.addGameEntity(new SwordBonus(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+				}
+				if (tab[i][j] == 3) {
+					universe.addGameEntity(new Floor(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+					universe.addGameEntity(new ShieldBonus(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+				}
+				if (tab[i][j] == 4) {
+					universe.addGameEntity(new Floor(canvas, j * SPRITE_SIZE, i * SPRITE_SIZE));
+					universe.addGameEntity(new HolyGrailBonus(canvas,j * SPRITE_SIZE, i * SPRITE_SIZE));
+				}
+			}
+		}
+
+		
+		GameSoldierObserver obs = new GameSoldierObserver(universe);
+
+		
+		GameSoldier player1 = new GameSoldier(canvas, "images/link.gif", obs, 26 * SPRITE_SIZE, 1 * SPRITE_SIZE, KeyEvent.VK_NUMPAD0  );
+		GameMovableDriverDefaultImpl driver1 = new GameMovableDriverDefaultImpl();
+		WarriorMoveStrategy keyStr = new WarriorMoveStrategy(KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT, KeyEvent.VK_UP, KeyEvent.VK_DOWN);
+		driver1.setStrategy(keyStr);
+		driver1.setmoveBlockerChecker(moveBlockerChecker);
+		canvas.addKeyListener(keyStr);
+		canvas.addKeyListener(player1);
+		player1.setDriver(driver1);
+		universe.addGameEntity(player1);
+
+		GameSoldier player2 = new GameSoldier(canvas, "images/link2.gif",obs, 1 * SPRITE_SIZE, 1 * SPRITE_SIZE, KeyEvent.VK_SPACE);
+		GameMovableDriverDefaultImpl driver2 = new GameMovableDriverDefaultImpl();
+		WarriorMoveStrategy keyStrPlayer2 = new WarriorMoveStrategy(KeyEvent.VK_D, KeyEvent.VK_Q, KeyEvent.VK_Z, KeyEvent.VK_S);
+		driver2.setStrategy(keyStrPlayer2);
+		driver2.setmoveBlockerChecker(moveBlockerChecker);
+		canvas.addKeyListener(keyStrPlayer2);
+		canvas.addKeyListener(player2);
+		player2.setDriver(driver2);
+		universe.addGameEntity(player2);
+	}
+	public GameLevelTwo(Game g) {
+		super(g);
+		canvas = g.getCanvas();
+	}
+	
+	
+	public void end() {
+		universe.allOneStepMoves();
+		gameBoard.paint();
+		super.end();
+	}
+}
